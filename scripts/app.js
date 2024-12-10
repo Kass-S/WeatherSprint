3bb7de756acf6cb059a9a381fb96c302c205785a
Kass-S <kassiespacek101@gmail.com>
2024-12-10 14:48:41 -0800

    made function change text

--- scripts/app.js
+++ scripts/app.js
@@ -2,27 +2,44 @@ import { APIKEY } from "./environment.js";
 
 let testBtn = document.getElementById('testBtn');
 
+let dailyForcastWeatherText = document.getElementById('dailyForcastWeatherText');
+let day1ForcastText = document.getElementById('day1ForcastText');
+let day2ForcastText = document.getElementById('day2ForcastText');
+let day3ForcastText = document.getElementById('day3ForcastText');
+let day4ForcastText = document.getElementById('day4ForcastText');
+let day5ForcastText = document.getElementById('day5ForcastText');
+
 testBtn.addEventListener('click', function(){
-    apiCallCurrent();
+    
+    //apiCallCurrent();
+    
     apiCallForcast();
 })
 
+let city = 'Lodi,US'
+
 function apiCallCurrent() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=38.1341&lon=-121.2722&appid=${APIKEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`)
     .then((response) => {
         return response.json()
     })
-    .then((data) => {
-        console.log(data);
+    .then((dataCurrent) => {
+        console.log(dataCurrent);
+        dailyForcastWeatherText.innerText = dataCurrent.weather[0].description;
     })
 }
 
 function apiCallForcast() {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=38.1341&lon=-121.2722&appid=${APIKEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`)
     .then((response) => {
         return response.json()
     })
     .then((data) => {
         console.log(data);
+        day1ForcastText.innerText = data;
+        // day2ForcastText.innerText = ;
+        // day3ForcastText.innerText = ;
+        // day4ForcastText.innerText = ;
+        // day5ForcastText.innerText = ;
     })
 }
