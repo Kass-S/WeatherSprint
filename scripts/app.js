0b3f52446c01ee079b29fb17581ef1ba1e38a061
Kass-S <kassiespacek101@gmail.com>
2024-12-10 15:51:26 -0800

    made daily weather image populate

--- scripts/app.js
+++ scripts/app.js
@@ -9,27 +9,31 @@ let day3ForcastText = document.getElementById('day3ForcastText');
 let day4ForcastText = document.getElementById('day4ForcastText');
 let day5ForcastText = document.getElementById('day5ForcastText');
 
+let weatherIconCurrent = document.getElementById('weatherIconCurrent');
+let weatherIconDay1 = document.getElementById('weatherIconDay1');
+
 testBtn.addEventListener('click', function(){
     
-    //apiCallCurrent();
+    apiCallCurrent();
     
-    apiCallForcast();
+    //apiCallForcast();
 })
 
 let city = 'Lodi,US'
 
 async function apiCallCurrent() {
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`);
-    const dataCurrent = await promise.json();
-    console.log(dataCurrent);
-    dailyForcastWeatherText.innerText = dataCurrent.weather[0].description;
+    const data = await promise.json();
+    console.log(data);
+    dailyForcastWeatherText.innerText = data.weather[0].description;
+    weatherIconCurrent.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 }
 
 async function apiCallForcast() {
     const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`);
-    const dataForecast = await promise.json();
-    console.log(dataForecast);
-    day1ForcastText.innerText = dataForecast;
+    const data = await promise.json();
+    console.log(data);
+    //day1ForcastText.innerText = data;
         // day2ForcastText.innerText = ;
         // day3ForcastText.innerText = ;
         // day4ForcastText.innerText = ;
